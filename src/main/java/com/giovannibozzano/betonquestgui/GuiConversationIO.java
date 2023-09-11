168dba7bcff0d7ed5f0d976b3cb9d0b41f2018bd
RaVen <66441550+RaVenInTheDark@users.noreply.github.com>
2023-09-11 17:00:39 +0100

    Handle users not having the mod / disabling gui.

--- src/main/java/com/giovannibozzano/betonquestgui/GuiConversationIO.java
+++ src/main/java/com/giovannibozzano/betonquestgui/GuiConversationIO.java
@@ -1,5 +1,6 @@
 package com.giovannibozzano.betonquestgui;
 
+import org.betonquest.betonquest.BetonQuest;
 import org.betonquest.betonquest.api.profiles.OnlineProfile;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -9,6 +10,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.betonquest.betonquest.conversation.Conversation;
 import org.betonquest.betonquest.conversation.ConversationIO;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,18 +23,52 @@ public class GuiConversationIO implements Listener, ConversationIO
     private int optionsIndex = 0;
     private String npcName;
     private boolean allowClose = false;
+    private boolean fallbackIO = false;
+    private ConversationIO io;
 
     public GuiConversationIO(Conversation conversation, OnlineProfile profile)
     {
         this.conversation = conversation;
         this.player = profile.getPlayer();
-        PacketHandler.sendPacketCreateGui(this.player);
+        if (!BetonQuestGui.INSTANCE.players.contains(profile.getPlayer().getName())) { 
+            fallbackIO = true;
+            final String rawConvIO = BetonQuest.getInstance().getPluginConfig().getString("default_conversation_IO", "menu,chest");
+            String convIO = "";
+
+            // check if all data is valid (or at least exist)
+            for (final String s : rawConvIO.split(",")) {
+                if (s.trim().equals("gui")) continue;
+                if (BetonQuest.getInstance().getConvIO(s.trim()) != null) {
+                    convIO = s.trim();
+                    break;
+                }
+            }
+
+            System.out.println("PLAYER NOT REGISTERED! Falling back to other config...");
+
+            final Class<? extends ConversationIO> convIOC = BetonQuest.getInstance().getConvIO(convIO);
+            try {
+                io = convIOC.getConstructor(Conversation.class, OnlineProfile.class).newInstance(conversation, profile);
+            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        if (!fallbackIO) {
+            PacketHandler.sendPacketCreateGui(this.player);
+        }
         Bukkit.getPluginManager().registerEvents(this, BetonQuestGui.INSTANCE.getBetonQuest());
     }
 
     @Override
     public void setNpcResponse(String npcName, String response)
     {
+        if (fallbackIO) {
+            io.setNpcResponse(npcName, response);
+            return;
+        }
         this.npcName = npcName;
         this.response = response.replace("%quester%", npcName).replace("%player%", this.player.getName()).replace('&', '§');
     }
@@ -40,6 +76,10 @@ public class GuiConversationIO implements Listener, ConversationIO
     @Override
     public void addPlayerOption(String option)
     {
+        if (fallbackIO) {
+            io.addPlayerOption(option);
+            return;
+        }
         this.optionsIndex += 1;
         this.options.put(this.optionsIndex, option.replace("%quester%", this.npcName).replace("%player%", this.player.getName()).replace('&', '§'));
     }
@@ -47,6 +87,10 @@ public class GuiConversationIO implements Listener, ConversationIO
     @Override
     public void display()
     {
+        if (fallbackIO) {
+            io.display();
+            return;
+        }
         if (this.response == null) {
             this.end();
             return;
@@ -63,6 +107,10 @@ public class GuiConversationIO implements Listener, ConversationIO
     @Override
     public void clear()
     {
+        if (fallbackIO) {
+            io.clear();
+            return;
+        }
         this.response = null;
         this.options.clear();
         this.optionsIndex = 0;
@@ -71,6 +119,9 @@ public class GuiConversationIO implements Listener, ConversationIO
     @Override
     public void end()
     {
+        if (fallbackIO) {
+            io.end();
+        }
         this.allowClose = true;
         if (this.response == null) {
             PacketHandler.sendPacketCloseGui(this.player);
